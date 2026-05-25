1b0d6f00c88cc0e5308dd4dee737be1c0046dd6d
ssugary <ssugary0@gmail.com>
2026-05-25 15:56:39 -0300

    feat: link RPI and ESP by pyPackets

--- drums/esp32/src/main.cpp
+++ drums/esp32/src/main.cpp
@@ -1,44 +1,101 @@
 #include <Arduino.h>
 
-const int piezoPin = 4;
+struct DrumPad {
+    int pin;
+    int threshold;
+    unsigned long lastHit;
+};
 
-const int threshold = 150;
+struct __attribute__((packed)) Packet {
+
+    uint8_t header;
+
+    uint8_t pad;
+
+    uint16_t value;
+
+    uint32_t timestamp;
+};
+
+const int n = 2;
+
+DrumPad pads[n] = {
+
+    {
+        2,
+        100,
+        0
+    },
+
+    {
+        4,
+        100,
+        0
+    },
+
+};
 
 const int scanTime = 12;         // ms procurando pico
 const int retriggerDelay = 120;  // ms
 
-unsigned long lastHit = 0;
-
 void setup() {
     Serial.begin(115200);
+    // Serial.println(sizeof(Packet));
 }
 
 void loop() {
 
-    int value = analogRead(piezoPin);
+    for(int i = 0; i < n; i++) {
 
-    // detecta início da pancada
-    if (
-        value > threshold &&
-        millis() - lastHit > retriggerDelay
-    ) {
+        int value = analogRead(
+            pads[i].pin
+        );
 
-        int peak = value;
+        // detecta início da pancada
+        if (
+            value > pads[i].threshold &&
+            millis() - pads[i].lastHit > retriggerDelay
+        ) {
 
-        unsigned long start = millis();
+            int peak = value;
 
-        // procura maior valor
-        while(millis() - start < scanTime) {
+            unsigned long start = millis();
 
-            value = analogRead(piezoPin);
+            // procura maior valor
+            while(millis() - start < scanTime) {
 
-            if(value > peak) {
-                peak = value;
+                value = analogRead(
+                    pads[i].pin
+                );
+
+                if(value > peak) {
+                    peak = value;
+                }
             }
+            
+            pads[i].lastHit = millis();
+
+            // envia:
+            // indice:valor
+            // exemplo:
+            // // 0:3200
+            // Serial.print(i);
+            // Serial.print(":");
+            // Serial.println(peak);
+
+            Packet packet;
+            packet.header = 0xAA;
+            packet.pad = i;
+            packet.value = peak;
+            packet.timestamp = micros();
+
+            
+
+            Serial.write(
+                (uint8_t*)&packet,
+                sizeof(Packet)
+            );
+
         }
-
-        lastHit = millis();
-
-        Serial.println(peak);
     }
 }
